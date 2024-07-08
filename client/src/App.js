5a6855d9f5b6fe2a973e678746f90f2f2e2d267f
maxklema <maxklema@gmail.com>
2024-07-07 23:07:58 -0400

    header, text, container, links, etc

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,10 @@ import TextArea from './components/text area/textarea';
 import DropDown from './components/drop down/dropdown';
 import RadioButton from './components/radio button/radiobutton';
 import CheckBoxes from './components/check boxes/checkboxes';
+import { STxt, MTxt, LTxt } from './components/text/text';
+import Header from './components/header/header';
+import Container from './components/container/container';
+import Link from './components/links/link';
 
 function App() {
 
@@ -16,17 +20,24 @@ function App() {
   const checkboxOptions = [{value: "Pizza", checked: true}, {value: 'Tacos', checked: true}, {value: 'Burger', checked: false}]
 
   return (
-   <div>
-      <InputButton text="Input Button" /><br></br><br></br>
-      <TextField type="text" onChange={setText} value={text} text='Username' />
-      <p>{text}</p>
-      <TextArea type="text" onChange={setArea} value={area} text='Biography' />
-      <p>{area}</p>
-      <DropDown hint="Select Role" options={options} text="Choose Car" /><br /><br />
-      <RadioButton options={radioOptions} /><br />
-      <CheckBoxes options={checkboxOptions} />
-      
-   </div>
+      <Container>
+        <Header />
+        <div className='content'>
+          <InputButton text="Input Button" /><br></br><br></br>
+          <TextField type="text" onChange={setText} value={text} text='Username' />
+          <p>{text}</p>
+          <TextArea type="text" onChange={setArea} value={area} text='Biography' />
+          <p>{area}</p>
+          <DropDown hint="Select Role" options={options} text="Choose Car" /><br /><br />
+          <RadioButton options={radioOptions} /><br />
+          <CheckBoxes options={checkboxOptions} /><br />
+          <STxt>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nisi explicabo ab exercitationem quidem, porro architecto ducimus tempora cum adipisci sunt eaque assumenda officia facilis repellendus, modi deserunt, dolor at?</STxt>
+          <MTxt>Medium Text</MTxt><br />
+          <LTxt>Large Text</LTxt><br />
+          <Link href="/" underline={true} >Link to URL</Link><br /><br />
+        </div>
+      </Container>
+
   );
 }
 
