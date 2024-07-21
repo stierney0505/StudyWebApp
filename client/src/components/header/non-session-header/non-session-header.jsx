113c3294bec9e5141beab74aaed6e43b8c168392
maxklema <maxklema@gmail.com>
2024-07-21 17:38:14 -0400

    Working on Dark Mode

--- client/src/components/header/non-session-header/non-session-header.jsx
+++ client/src/components/header/non-session-header/non-session-header.jsx
@@ -1,16 +1,32 @@
 import './non-session-header.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from '../../links/link';
-import { Menu } from "@carbon/icons-react";
+import { Menu, LightFilled, AsleepFilled } from "@carbon/icons-react";
 import TextField from '../../text-field/text-field';
 import InputButton from '../../button/button';
 import Logo from './../../../../assets/logo.svg'
 
 const NoSessionHeader = () => {
 
+    const [darkMode, setDarkMode] = useState("");
     const [searchValue, setSearchValue] = useState('');
     const [mobileNavDisplay, setMobileNavDisplay] = useState(false);
+    
     let mobile_header_menu = document.getElementById('mobile-hrefs');
+
+    useEffect(() => {
+        let darkModeValue = localStorage.getItem("darkMode");
+
+        if (!darkModeValue){
+            localStorage.setItem('darkMode', false);
+            setDarkMode(false);
+        } else {
+            setDarkMode(darkModeValue);
+        }
+
+        document.body.setAttribute('data-theme', `${darkModeValue}`);
+
+    }, []);
 
     const openMenu = () => {
         // mobile_header_menu.style.display = "block";
@@ -22,6 +38,13 @@ const NoSessionHeader = () => {
         setMobileNavDisplay(false);
     }
 
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+        localStorage.setItem("darkMode", darkMode);
+        document.body.setAttribute('data-theme', `${darkMode}`);
+
+
+    }
 
 
     return (
@@ -33,9 +56,12 @@ const NoSessionHeader = () => {
                 <TextField fieldId="search-box" type="text" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} placeholder="Search Sets" />
                 <Link href="/login" id="log-in-link" underline={true} >Log In</Link>
                 <InputButton text={"Get Started"} URL="/create-account" id="sign-up-button"/>
+                <Link href="#" id="toggle-dark-mode" onClick={toggleDarkMode}>
+                    { !darkMode ? <AsleepFilled size={25}/> : <LightFilled size={25}/>}
+                </Link>
             </div>
             <div className='hamburger-menu'>
-                <Link href="#" id="hamburger-icon" onClick={openMenu}><Menu size={25}/></Link>
+                <Link href="#" id="hamburger-icon" onClick={openMenu}><Menu size={25} /></Link>
             </div>
         
             {/* For Mobile */}
