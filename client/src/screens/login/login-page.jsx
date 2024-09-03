e040ed52aa0fba04f9368935ac83bd224777036b
lukebushur <luke7ajb@gmail.com>
2024-09-02 22:29:18 -0400

    refactored login page to use CSS modules, error messages are displayed below each field

--- client/src/screens/login/login-page.jsx
+++ client/src/screens/login/login-page.jsx
@@ -1,7 +1,7 @@
-import TextField from "../../components/text-field/text-field";
+import OutlinedTextField from "../../components/outlined-text-field/outlined-text-field";
 import { LTxt, MTxt, STxt } from "../../components/text/text";
 import InputButton from "../../components/button/button";
-import './login-page.css';
+import styles from './login-page.module.css';
 import Link from "../../components/links/link";
 import { useForm } from "react-hook-form";
 import { Helmet } from "react-helmet";
@@ -15,53 +15,59 @@ const LoginPage = () => {
         formState: { errors },
     } = useForm();
 
+    console.log(errors);
+
+
     const onSubmit = (data) => {
-        
+
         console.log(data.username);
 
     }
 
     return (
-        <>  
+        <>
             <Helmet>
                 <title>Log In</title>
             </Helmet>
-            <div className="login-container">
-                <form method="" onSubmit={handleSubmit(onSubmit)} className="login-form">
-                    <LTxt id="header-main">Study Web App</LTxt>
-                    <MTxt id="header-secondary">Log in</MTxt>
-                    <div className="login-fields">
-                        <TextField
-                            id={errors.username ? 'lg_username_id_error' : 'lg_username_id'}
-                            fieldId={errors.username ? 'lg-username-error' : 'lg-username'}
-                            text="Username"
-                            name="username"  // Update this to match the register name
-                            type="text"
-                            register={register('username', { 
-                                required: 'Username is Required',
-                                minLength: { value: 3, message: 'Username must be at least 3 characters long' },
-                                maxLength: { value: 22, message: 'Username cannot exceed 22 characters' } 
-                            })}
-                        />
-                        <TextField
-                            id={errors.password ? 'lg_password_id_error' : ''}
-                            fieldId={errors.password ? 'lg-password-error' : 'lg-password'}  
-                            text={'Password'}
-                            name="password"
-                            type={'password'}
-                            register={register('password', { 
-                                required: 'Password is Required',
-                                minLength: { value: 3, message: 'Password must be at least 3 characters long' },
-                                maxLength: { value: 22, message: 'Password cannot exceed 22 characters' }  
-                            })}
-                        />
-                        {(errors.username) && <STxt id="error-text">{errors.username.message}</STxt>}
-                        {(errors.password && !errors.username) && <STxt id="error-text">{errors.password.message}</STxt>}
+            <div className={styles['login-container']}>
+                <form method="" onSubmit={handleSubmit(onSubmit)} className={styles['login-form']}>
+                    <LTxt id={styles['header-main']}>Study Web App</LTxt>
+                    <MTxt id={styles['header-secondary']}>Log in</MTxt>
+                    <div className={styles['login-fields']}>
+                        <div className={styles['text-field']}>
+                            <OutlinedTextField
+                                fieldId={'lg-username'}
+                                text="Username"
+                                name="username"  // Update this to match the register name
+                                type="text"
+                                register={register('username', {
+                                    required: 'Username is required',
+                                    minLength: { value: 3, message: 'Username must be at least 3 characters long' },
+                                    maxLength: { value: 22, message: 'Username cannot exceed 22 characters' }
+                                })}
+                                isError={errors.username}
+                            />
+                            {(errors.username) && <STxt className={styles['error-text']}>{errors.username.message}</STxt>}
+                        </div>
+                        <div className={styles['text-field']}>
+                            <OutlinedTextField
+                                fieldId={'lg-password'}
+                                text="Password"
+                                name="password"
+                                type="password"
+                                register={register('password', {
+                                    required: 'Password is required',
+                                    minLength: { value: 3, message: 'Password must be at least 3 characters long' },
+                                    maxLength: { value: 22, message: 'Password cannot exceed 22 characters' }
+                                })}
+                                isError={errors.password}
+                            />
+                        {(errors.password) && <STxt className={styles['error-text']}>{errors.password.message}</STxt>}
+                        </div>
                     </div>
-                    <InputButton id="submit-button" text="Log In" type="submit" />
-                    <STxt id="or-header">─── Or ───</STxt>
-                    <Link href="/create-account" id="create-account-link"><STxt>Don&apos;t have an account? Create one</STxt></Link>
-
+                    <InputButton id={styles['submit-button']} text="Log In" type="submit" />
+                    <STxt id={styles['or-header']}>─── Or ───</STxt>
+                    <Link href="/create-account" id={styles['create-account-link']}><STxt>Don&apos;t have an account? Create one</STxt></Link>
                 </form>
             </div>
         </>
