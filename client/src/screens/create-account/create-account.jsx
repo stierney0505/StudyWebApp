a8744b2dc5400f3b2e415bfbaf449a852bc4a91d
lukebushur <luke7ajb@gmail.com>
2024-09-02 22:52:32 -0400

    refactored to use react hook form, added form validation

--- client/src/screens/create-account/create-account.jsx
+++ client/src/screens/create-account/create-account.jsx
@@ -1,28 +1,39 @@
-import { useId, useState } from 'react';
-import axios from 'axios';
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "_" }] */
+
+import { useId } from 'react';
+import { useForm } from 'react-hook-form';
 import { Helmet } from 'react-helmet';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 import styles from './create-account.module.css';
 
-import TextField from '../../components/text-field/text-field';
+import OutlinedTextField from '../../components/outlined-text-field/outlined-text-field';
 import InputButton from '../../components/button/button';
 import Link from '../../components/links/link';
+import SessionHeader from '../../components/header/session-header/session-header';
 
 const CreateForm = () => {
   const id = useId();
   const navigate = useNavigate();
-  const [form, setForm] = useState({
-    fname: '',
-    lname: '',
-    email: '',
-    password: '',
-    confirmPassword: ''
-  });
 
-  function createAccount(event) {
-    event.preventDefault();
-    axios.post(`${import.meta.env.VITE_SERVER_URI}/api/users`, form)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  console.log(errors);
+
+  function onSubmit(data) {
+    const { confirmPassword: _, ...submissionData } = data;
+    console.log(submissionData);
+
+    axios.post(
+      `${import.meta.env.VITE_SERVER_URI}/api/users`,
+      submissionData,
+      { withCredentials: true }
+    )
       .then(function (response) {
         if (response.data.success) {
           navigate('/dashboard')
@@ -35,80 +46,115 @@ const CreateForm = () => {
 
   return (
     <>
-      <Helmet>
-          <title>Create Account</title>
-      </Helmet>
-      <h1 className={ [styles.heading, styles['poppins-bold']].join(' ') }>Create an Account</h1>
-      <form method='post' onSubmit={createAccount} >
-        <div className={ [styles['name-group'], styles['form-line']].join(' ') }>
-          <TextField
-            fieldId={id + '-fname'}
-            text={'First Name'}
-            name={id + '-fname'}
-            type={'text'}
-            value={form.fname}
-            onChange={e => setForm({
-              ...form,
-              fname: e.target.value
-            })}
-          />
-          <TextField
-            fieldId={id + '-lname'}
-            text={'Last Name'}
-            name={id + '-lname'}
-            type={'text'}
-            value={form.lname}
-            onChange={e => setForm({
-              ...form,
-              lname: e.target.value
-            })}
-          />
+      <h1 className={[styles.heading, styles['poppins-bold']].join(' ')}>Create an Account</h1>
+      <form onSubmit={handleSubmit(onSubmit)} >
+        <div className={[styles['name-group'], styles['form-line']].join(' ')}>
+          <div className={styles['text-field']}>
+            <OutlinedTextField
+              fieldId={id + '-firstName'}
+              name="firstName"
+              text="First Name"
+              type="text"
+              register={register('firstName', {
+                required: 'First Name is required',
+                minLength: { value: 3, message: 'First Name must be at least 3 characters long', },
+                maxLength: { value: 22, message: 'First Name cannot exceed 22 characters', },
+              })}
+              isError={errors.firstName}
+            />
+            {(errors.firstName) && <p className={styles['error-text']}>{errors.firstName.message}</p>}
+          </div>
+          <div className={styles['text-field']}>
+            <OutlinedTextField
+              fieldId={id + '-lastName'}
+              name="lastName"
+              text="Last Name"
+              type="text"
+              register={register('lastName', {
+                required: 'Last Name is required',
+                minLength: { value: 3, message: 'Last Name must be at least 3 characters long', },
+                maxLength: { value: 22, message: 'Last Name cannot exceed 22 characters', },
+              })}
+              isError={errors.lastName}
+            />
+            {(errors.lastName) && <p className={styles['error-text']}>{errors.lastName.message}</p>}
+          </div>
         </div>
         <div className={styles['form-line']}>
-          <TextField
-            fieldId={id + '-email'}
-            text={'Email'}
-            name={id + '-email'}
-            type={'email'}
-            value={form.email}
-            onChange={e => setForm({
-              ...form,
-              email: e.target.value
-            })}
-          />
+          <div className={styles['text-field']}>
+            <OutlinedTextField
+              fieldId={id + '-email'}
+              name="email"
+              text="Email"
+              type="email"
+              register={register('email', {
+                required: 'Email is required',
+                pattern: {
+                  value: /^\S+@\S+\.\S+$/,
+                  message: 'Entered value does not match email format',
+                },
+              })}
+              isError={errors.email}
+            />
+            {(errors.email) && <p className={styles['error-text']}>{errors.email.message}</p>}
+          </div>
         </div>
         <div className={styles['form-line']}>
-          <TextField
-            fieldId={id + '-password'}
-            text={'Password'}
-            name={id + '-password'}
-            type={'password'}
-            value={form.password}
-            onChange={e => setForm({
-              ...form,
-              password: e.target.value
-            })}
-          />
+          <div className={styles['text-field']}>
+            <OutlinedTextField
+              fieldId={id + '-password'}
+              text="Password"
+              name="password"
+              type="password"
+              register={register('password', {
+                required: 'Password is required',
+                minLength: {
+                  value: 8,
+                  message: 'Password must be at least 8 characters long',
+                },
+                validate: {
+                  atLeastOneDigit: (value) => {
+                    return /\d/.test(value) || 'Password must contain at least 1 digit';
+                  },
+                  atLeastOneLetter: (value) => {
+                    return /[a-zA-Z]/.test(value) || 'Password must contain at least 1 letter';
+                  },
+                  atLeastOneSpecial: (value) => {
+                    return /\W/.test(value) || 'Password must contain at least 1 special character';
+                  },
+                },
+              })}
+              isError={errors.password}
+            />
+            {(errors.password) && <p className={styles['error-text']}>{errors.password.message}</p>}
+          </div>
         </div>
         <div className={styles['form-line']}>
-          <TextField
-            fieldId={id + '-confirmPassword'}
-            text={'Confirm Password'}
-            name={id + '-confirmPassword'}
-            type={'password'}
-            value={form.confirmPassword}
-            onChange={e => setForm({
-              ...form,
-              confirmPassword: e.target.value
-            })}
-          />
+          <div className={styles['text-field']}>
+            <OutlinedTextField
+              fieldId={id + '-confirmPassword'}
+              text="Confirm Password"
+              name="confirmPassword"
+              type="password"
+              register={register('confirmPassword', {
+                required: 'Confirm Password is required',
+                validate: {
+                  matchPassword: (value, formValues) => {
+                    return value === formValues.password || 'Confirm Password must match Password';
+                  },
+                },
+              })}
+              isError={errors.confirmPassword}
+            />
+            {(errors.confirmPassword) && <p className={styles['error-text']}>{errors.confirmPassword.message}</p>}
+          </div>
         </div>
         <div className={styles['login-link-line']}>
           <p className={styles['login-text']}>Already have an account?</p>
-          <Link href={'/login'} underline={true}>Click here!</Link>
+          <Link href="/login" underline={true}>Click here!</Link>
         </div>
         <div className={styles['button-line']}>
-          <InputButton type={'submit'} text={'Submit'} />
+          <InputButton type="submit" text="Create" />
         </div>
       </form>
     </>
@@ -116,11 +162,17 @@ const CreateForm = () => {
 };
 
 const CreateAccount = () => (
-  <div className={styles['screen-container']}>
-    <div className={styles['form-container']}>
-      <CreateForm />
+  <>
+    <Helmet>
+      <title>Create Account</title>
+    </Helmet>
+    <SessionHeader />
+    <div className={styles['screen-container']}>
+      <div className={styles['form-container']}>
+        <CreateForm />
+      </div>
     </div>
-  </div>
+  </>
 );
 
 export default CreateAccount;
