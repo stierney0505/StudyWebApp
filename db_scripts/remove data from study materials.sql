c5a4fcb5dd8a48ea646e82b79524469938eead30
Sean Tierney <stierney0505@gmail.com>
2024-09-03 20:53:56 -0500

    Set up SQL tables for creating study materials

---
+++ db_scripts/remove data from study materials.sql
@@ -0,0 +1,12 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+drop table study_material_tag_mapping;
+drop table study_material_tags;
+drop table study_material;
+drop table flashcards;
+drop table study_material_flashcards_join;
+drop table user_study_material_join;
+drop procedure insert_study_material;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
